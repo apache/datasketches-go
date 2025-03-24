d9d187f8b34c1e0c7799bb452a308eeaab2b15a3
qasim.khan <qasim.khan@datadoghq.com>
2025-03-24 16:07:29 +0100

    refactor and add utils to common

--- common/utils.go
+++ common/utils.go
@@ -17,10 +17,62 @@
 
 package common
 
+import (
+	"math"
+	"math/bits"
+)
+
 const (
-	_DEFAULT_SERDE_HASH_SEED = uint64(9001)
+	defaultSerdeHashSeed = uint64(9001)
+	InverseGoldenU64     = 0x9e3779b97f4a7c13
 )
 
 func checkBounds(offset int, reqLen int, memCap int) bool {
 	return !((offset | reqLen | (offset + reqLen) | (memCap - (offset + reqLen))) < 0)
+}
+
+func PowerSeriesNextDouble(ppb int, curPoint float64, roundToLong bool, logBase float64) float64 {
+	// If curPoint < 1.0, force cur to 1.0
+	cur := curPoint
+	if cur < 1.0 {
+		cur = 1.0
+	}
+
+	// Compute the "generating index" (gi) by rounding logBaseOfX(logBase, cur)*ppb
+	gi := math.Round(logBaseOfX(logBase, cur) * float64(ppb))
+
+	for {
+		// Increment gi, compute n = logBase^(gi/ppb)
+		gi += 1.0
+		n := math.Pow(logBase, gi/float64(ppb))
+
+		// If roundToLong is true, round to nearest integer; otherwise keep the float
+		var next float64
+		if roundToLong {
+			next = math.Round(n)
+		} else {
+			next = n
+		}
+
+		// Repeat until next > cur
+		if next > cur {
+			return next
+		}
+	}
+}
+
+func logBaseOfX(base, x float64) float64 {
+	return math.Log(x) / math.Log(base)
+}
+
+func CeilingPowerOf2(n int) int {
+	if n <= 1 {
+		return 1
+	}
+	const topIntPwrOf2 = 1 << 30
+	if n >= topIntPwrOf2 {
+		return topIntPwrOf2
+	}
+	x := (n - 1) << 1
+	return 1 << (bits.Len32(uint32(x)) - 1)
 }

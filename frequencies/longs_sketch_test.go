213dc5c5fa6401eeba53adfee00ef6faf0ace454
proost <jwalag87@gmail.com>
2026-09-05 15:27:44 +0900

    perf: reduce temp allocation in serialization

--- frequencies/longs_sketch_test.go
+++ frequencies/longs_sketch_test.go
@@ -23,8 +23,9 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/apache/datasketches-go/internal"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/apache/datasketches-go/internal"
 )
 
 func TestFrequentItemsStringSerial(t *testing.T) {
@@ -642,5 +643,38 @@ func BenchmarkLongSketch(b *testing.B) {
 	assert.NoError(b, err)
 	for i := 0; i < b.N; i++ {
 		sketch.Update(int64(i))
+	}
+}
+
+var toSliceSink []byte
+
+func BenchmarkLongsSketchToSlice(b *testing.B) {
+	cases := []struct {
+		lgMaxMapSize int
+		numItems     int
+	}{
+		{6, 48},
+		{10, 768},
+		{14, 12288},
+		{18, 196608},
+	}
+
+	for _, c := range cases {
+		sk, err := NewLongsSketch(c.lgMaxMapSize, _LG_MIN_MAP_SIZE)
+		if err != nil {
+			b.Fatal(err)
+		}
+		for i := 0; i < c.numItems; i++ {
+			if err := sk.UpdateMany(int64(i), int64(i+1)); err != nil {
+				b.Fatal(err)
+			}
+		}
+
+		b.Run(fmt.Sprintf("active=%d", sk.GetNumActiveItems()), func(b *testing.B) {
+			b.ReportAllocs()
+			for b.Loop() {
+				toSliceSink = sk.ToSlice()
+			}
+		})
 	}
 }
